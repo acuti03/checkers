d2bc1e7a5bf38fc3b0dd1bc85506c9d7daee5c25
Simone Acuti <acutisimone@gmail.com>
2023-02-09 00:05:08 +0100

    functions for the pawn and to interrupt the game are missing

--- main.c
+++ main.c
@@ -4,20 +4,42 @@
 int main(){
     PlayGround playGround;
     Status status = InProgress;
-    int player;
-    int a;
+    char player;
+    int rowS, colS, rowF, colF;
+    int cnt = 2;
 
-    system("clear");
+
+//  initialise ncurses and colors
+    initscr();
+    start_color();
+
+//  generate the playground
     createPlayGround(&playGround);
-    printPlayGround(&playGround);
-    printInsertBox();
 
-    //do{
+//  cycle for game
+    do{
+        clear();
+        player = playerSelector(cnt);
+        printPlayGround(&playGround);
 
-    //}while(status == InProgress);
+        if(eatCheck(&playGround, &rowS, &colS, &rowF, &colF, player)){
+            makeMove(rowS, colS, rowF, colF, &playGround);
+            printw("you can eat from X: %d Y: %d to X: %d Y: %d", rowS, colS, rowF, colF);
+            getch();
+        }
+        else{
+            inputBox(&rowS, &colS, &rowF, &colF, player, &playGround);
+            makeMove(rowS, colS, rowF, colF, &playGround);
+        }
+        
+        display(&playGround, player);
+        getch();
+        refresh();
+        cnt++;
+    }while(status == InProgress); // status == INPROGRESS
 
 
-    printf("\n\n");
+    getch();
+    endwin();
     return 0;
-    
 }
